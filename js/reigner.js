c32f94e7e79f7955d714ece098088aa6f74eec10
INNOCENT CHIDERA OKWOR <innocent123chidera@gmail.com>
2025-06-26 00:18:14 +0100

    your message

---
+++ js/reigner.js
@@ -0,0 +1,26 @@
+
+   <section>
+    <div class=" h-screen">
+        <h2 class="text-center font-bold">Sermons</h2>
+        <div class=" text-center justify-center bg-[url(media/man.jpeg)] bg-cover bg-center h-64">
+            <p class="text-center italic w-60"> "mattew 3:3 Deliverance can be preached"
+            <br>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur maiores facilis animi?
+        </p>
+        </div>
+        <div class="justify-center align-center w-full max-w-xl relative overflow-hidden rounded-xl shadow-lg">
+            <div class="" id="slider">
+                    <div class="flex" id="slides">
+                        <img src="media/1.png" alt="" class="">
+                        <img src="media/2.png" alt="" class="">
+                        <img src="media/3.png" alt="" class="">
+                         <button class="bg-white-" id="prev" onclick="prev()">&#10094</button>
+                    </div>
+           
+            <button class="next" id="next" onclick="next()">&#10095</button>
+        </div>
+    </div>
+   </section>
+   <script src="js/reigner.js"></script>
+</body>
+</html>
